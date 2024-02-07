a16e437e8aea71e97db3d94c5ba3a7d39857ee7e
tchami <131756888+tchamianest@users.noreply.github.com>
2024-02-07 23:46:56 +0200

    try typescript

--- src/kalisa.js
+++ src/kalisa.js
@@ -1,17 +1,56 @@
-"use strict";
-var _a;
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Object = void 0;
-exports._b = _a = [];
-function Kalisa(param) {
-    var ob = {
-        name: "kalisa daniel",
-        phone: "kibam",
-        age: 21,
-        sex: "male",
-        kibaruma: "fatakumavuta",
-    };
-    exports.Object.push(ob);
-    return ob;
+// interface Admin {
+//   name: string;
+//   phone: string;
+//   age: number;
+//   sex: string;
+// }
+// export const Object []: Admin = [];
+// function Kalisa(param: Admin): Admin {
+//   let ob = {
+//     name: "kalisa daniel",
+//     phone: "kibam",
+//     age: 21,
+//     sex: "male",
+//     kibaruma: "fatakumavuta",
+//   };
+//   Object.push(ob)
+//   return ob;
+// }
+// console.log(Kalisa);
+// /////////////////////////////////////////////////////////////////////
+// // check docs
+// let obj:any={x:0};// for this type we can not have any error
+// obj.foo();
+// obj.kalisa='daniel';
+//// do function that have the
+// const greet = function (name: string): string {
+//   let final = `the first name is ` + name.toUpperCase();
+//   return final;
+// };
+// greet("tchami anest");
+///////////////////////////////////////////////////////////////////
+//////////if you want to return the promise
+// async function kalisadan(): Promise<number> {
+//   return 23;
+// }
+// const names: string[] = ["dani", "samusure", "kigwede"];
+// names.forEach(function (s) {
+//   console.log(s.toUpperCase());
+// });
+//////////////////////////////////////////////////////////////////
+////////print card
+function Printnum(pts) {
+    console.log("kalisa number " + pts.x);
+    console.log("nammes is the ".concat(pts.y));
 }
-console.log(Kalisa);
+Printnum({ x: "32", y: 56 });
+////////THE LAST FUNCTION
+function Primitive(ob) {
+    var _a;
+    console.log(ob.firstname.toUpperCase());
+    ///CHECK FIRST
+    if (ob.last !== undefined) {
+        console.log(ob.last.toUpperCase());
+    }
+    console.log((_a = ob.last) === null || _a === void 0 ? void 0 : _a.toUpperCase());
+}
--- src/kalisa.ts
+++ src/kalisa.ts
@@ -1,22 +1,70 @@
-interface Admin {
-  name: string;
-  phone: string;
-  age: number;
-  sex: string;
+// interface Admin {
+//   name: string;
+//   phone: string;
+//   age: number;
+//   sex: string;
+// }
+
+// export const Object []: Admin = [];
+
+// function Kalisa(param: Admin): Admin {
+//   let ob = {
+//     name: "kalisa daniel",
+//     phone: "kibam",
+//     age: 21,
+//     sex: "male",
+//     kibaruma: "fatakumavuta",
+//   };
+//   Object.push(ob)
+//   return ob;
+// }
+
+// console.log(Kalisa);
+
+// /////////////////////////////////////////////////////////////////////
+// // check docs
+// let obj:any={x:0};// for this type we can not have any error
+// obj.foo();
+// obj.kalisa='daniel';
+
+//// do function that have the
+// const greet = function (name: string): string {
+//   let final = `the first name is ` + name.toUpperCase();
+//   return final;
+// };
+
+// greet("tchami anest");
+
+///////////////////////////////////////////////////////////////////
+//////////if you want to return the promise
+
+// async function kalisadan(): Promise<number> {
+//   return 23;
+// }
+
+// const names: string[] = ["dani", "samusure", "kigwede"];
+// names.forEach(function (s) {
+//   console.log(s.toUpperCase());
+// });
+
+//////////////////////////////////////////////////////////////////
+////////print card
+function Printnum(pts: { x: string; y: number }): void {
+  console.log(`kalisa number ` + pts.x);
+  console.log(`nammes is the ${pts.y}`);
 }
 
-export const Object []: Admin = [];
+Printnum({ x: "32", y: 56 });
 
-function Kalisa(param: Admin): Admin {
-  let ob = {
-    name: "kalisa daniel",
-    phone: "kibam",
-    age: 21,
-    sex: "male",
-    kibaruma: "fatakumavuta",
-  };
-  Object.push(ob)
-  return ob;
+////////THE LAST FUNCTION
+
+function Primitive(ob: { firstname: string; last?: string }) {
+  console.log(ob.firstname.toUpperCase());
+
+  ///CHECK FIRST
+  if (ob.last !== undefined) {
+    console.log(ob.last.toUpperCase());
+  }
+
+  console.log(ob.last?.toUpperCase());
 }
-
-console.log(Kalisa);
